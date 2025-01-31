811ec73da14d9afd610802ae31cec8e3a66c7087
Simon Wright <simon@pushface.org>
2025-01-31 16:03:00 +0000

    GCC, GCC for Alire, aarch64, 15
      * gcc-for-alire.sh
      * gcc.sh

--- gcc-for-alire.sh
+++ gcc-for-alire.sh
@@ -1,13 +1,12 @@
-# Uses an existing compiler to build one for Alire.
+# Build using extant GCC.
 
 script_loc=`cd $(dirname $0) && pwd -P`
 
 . $script_loc/common.sh
 
-XCODE=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
-CLT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+XCODE=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.sdk
+CLT=/Library/Developer/CommandLineTools/SDKs/MacOSX14.sdk
 
-SDKROOT=$(xcrun --show-sdk-path)
 BUGURL=https://github.com/simonjwright/building-gcc-macos-native
 
 echo "BUILDING THE COMPILER IN $PREFIX"
@@ -15,72 +14,18 @@ echo "BUILDING THE COMPILER IN $PREFIX"
 set -eu
 rm -rf *
 
-$GCC_SRC/configure                                                       \
-    --prefix=$PREFIX                                                     \
-    --without-libiconv-prefix                                            \
-    --disable-libmudflap                                                 \
-    --disable-libstdcxx-pch                                              \
-    --disable-libsanitizer                                               \
-    --disable-libcc1                                                     \
-    --disable-libcilkrts                                                 \
-    --disable-multilib                                                   \
-    --disable-nls                                                        \
-    --enable-languages=c,c++,ada                                         \
-    --host=$BUILD                                                        \
-    --target=$BUILD                                                      \
-    --build=$BUILD                                                       \
-    --without-isl                                                        \
-    --with-build-sysroot=$SDKROOT                                        \
-    --with-sysroot=                                                      \
-    --with-specs="%{!sysroot=*:--sysroot=%:if-exists-else($XCODE $CLT)}" \
-    --with-bugurl=$BUGURL                                                \
-    --$BOOTSTRAP-bootstrap                                               \
-    --enable-host-pie                                                    \
-    CFLAGS=-Wno-deprecated-declarations                                  \
-    CXXFLAGS=-Wno-deprecated-declarations
+$GCC_SRC/configure                                                      \
+    --prefix=$PREFIX/gcc                                                \
+    --enable-languages=c,c++,ada                                        \
+    --build=$BUILD                                                      \
+    --with-sysroot=$SDKROOT/../MacOSX14.sdk                             \
+    --with-specs="%{!-sysroot:--sysroot=%:if-exists-else($XCODE $CLT)}" \
+    --with-bugurl=$BUGURL                                               \
+    --$BOOTSTRAP-bootstrap
+
+    # CFLAGS=-Wno-deprecated-declarations                                  \
+    # CXXFLAGS=-Wno-deprecated-declarations
 
 make -w -j$CORES
 
 make -w -j$CORES install
-
-# Install the shim for ld
-# Find the full path name for the C compiler's location
-tool_path=$(dirname $($PREFIX/bin/gcc --print-prog-name=cc1))
-echo "Installing in $tool_path"
-
-# Write our shim there. If we're running a pre-15 SDK, there won't be
-# an ld-classic, so use the standard linker.
-#
-# Need to keep a lookout for SDKs version > 15.
-#
-# We quote the EOF to avoid parameter substitution while writing the
-# document.
-cat >$tool_path/ld <<'EOF'
-#!/bin/sh
-
-classic=$(xcrun --find ld-classic 2>/dev/null) || true
-
-if [ -n "$classic" ]; then
-    exec $classic "$@"
-else
-    exec ld "$@"
-fi
-
-EOF
-
-# It needs to be executable!
-chmod +x $tool_path/ld
-
-# Fix up rpaths.
-for exe in $PREFIX/bin/*; do
-    if [[ $(file $exe) == *executable* ]]; then
-        bash $script_loc/fix_executable_rpaths.sh $exe
-    fi
-done
-
-for lib in $PREFIX/lib/*.dylib; do
-    if [[ $(file $lib) == *shared\ library* ]]; then
-        bash $script_loc/fix_library_rpaths.sh $lib
-    fi
-done
-
--- gcc.sh
+++ gcc.sh
@@ -14,9 +14,6 @@ echo "BUILDING THE COMPILER IN $PREFIX"
 set -eu
 rm -rf *
 
-BASE_COMPILER=/Volumes/Miscellaneous3/x86_64/gcc-14.2.0-clt-x86_64
-BASE_SYSROOT=/Library/Developer/CommandLineTools-14.3/SDKs/MacOSX13.sdk
-
 $GCC_SRC/configure                                                      \
     --prefix=$PREFIX                                                    \
     --enable-languages=c,c++,ada                                        \
@@ -24,9 +21,7 @@ $GCC_SRC/configure                                                      \
     --with-sysroot=$SDKROOT/../MacOSX14.sdk                             \
     --with-specs="%{!-sysroot:--sysroot=%:if-exists-else($XCODE $CLT)}" \
     --with-bugurl=$BUGURL                                               \
-    --$BOOTSTRAP-bootstrap                                              \
-    CC="$BASE_COMPILER/bin/gcc --sysroot=$BASE_SYSROOT"                 \
-    CXX="$BASE_COMPILER/bin/g++ --sysroot=$BASE_SYSROOT"
+    --$BOOTSTRAP-bootstrap
 
 make -w -j$CORES
 
